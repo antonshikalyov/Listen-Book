306609021f6ff84af57058ce739e076c88dcbb54
VARYAG 🎮 <anton.shikalyov@gmail.com>
2024-05-08 19:42:51 +0300

    Configured two seekbars, textViews for updating time and thread for playing music that allow update time and seekbars in real time. Although wecan rewind track with seekbar and set volume with seek bar for music

---
+++ ListenBook/app/build/intermediates/cxx/Debug/4h1g1l52/logs/x86_64/build_command_listenbook.bat
@@ -0,0 +1,5 @@
+@echo off
+"D:\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "D:\\AndroidStudioProjects\\reserve\\audiobook\\ListenBook\\app\\.cxx\\Debug\\4h1g1l52\\x86_64" ^
+  listenbook
