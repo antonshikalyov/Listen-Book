cf9df2bf75e31040f64f9a7d4541f198307e68a4
VARYAG 🎮 <anton.shikalyov@gmail.com>
2024-05-08 19:45:56 +0300

    rebase files

--- ListenBook/app/build/intermediates/cxx/Debug/4h1g1l52/logs/armeabi-v7a/build_command_listenbook.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"D:\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\AndroidStudioProjects\\reserve\\audiobook\\ListenBook\\app\\.cxx\\Debug\\4h1g1l52\\armeabi-v7a" ^
-  listenbook
